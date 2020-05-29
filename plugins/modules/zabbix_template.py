9fd26d3bce5ce34519ec072b08d49209166cdb16
Dusan Matejka <matejkadusan32@gmail.com>
2020-05-29 17:19:33 +0200

    zabbix_template no longer imports template from file with check mode

--- plugins/modules/zabbix_template.py
+++ plugins/modules/zabbix_template.py
@@ -559,6 +559,9 @@ class Template(object):
             self._module.fail_json(msg='Invalid XML provided', details=to_native(e), exception=traceback.format_exc())
 
     def import_template(self, template_content, template_type='json'):
+        if self._module.check_mode:
+            self._module.exit_json(changed=True)
+
         # rules schema latest version
         update_rules = {
             'applications': {
