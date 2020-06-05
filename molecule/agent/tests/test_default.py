2e2142dfdb5e8cbd2a7ecc86bbc5686288e236c2
ericsysmin <eric.sysmin@gmail.com>
2020-06-05 14:14:05 -0700

    add a test for zabbix.agent

---
+++ molecule/agent/tests/test_default.py
@@ -0,0 +1,20 @@
+import os
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_service(host):
+    service = host.service('docker')
+
+    assert service.is_running
+    assert service.is_enabled
+
+
+def test_hosts_file(host):
+    f = host.file('/etc/hosts')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
