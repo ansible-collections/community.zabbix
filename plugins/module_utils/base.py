514679564259b6192ded294e42efadeb1d216398
Markus Fischbacher <453368+rockaut@users.noreply.github.com>
2020-06-28 17:20:18 +0200

    Add initial bare minimum module_utils (#107)
    * initial tests
    
    * fix broken (self)recursion
    
    * adding login
    
    * fix sanity tests
    
    * fix import sanity failing
    
    * fix back compat errors
    
    * fix missing imports

---
+++ plugins/module_utils/base.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+import traceback
+
+from ansible.module_utils.basic import AnsibleModule, missing_required_lib
+from distutils.version import LooseVersion
+
+from ansible_collections.community.zabbix.plugins.module_utils.wrappers import ZapiWrapper
+
+
+class ZabbixBase(object):
+    """
+    The base class for deriving off module classes
+    """
+    def __init__(self, module, zbx=None, zapi_wrapper=None):
+        self._module = module
+
+        if zapi_wrapper is None:
+            self._zapi_wrapper = ZapiWrapper(module, zbx)
+        else:
+            self._zapi_wrapper = zapi_wrapper
+
+        # include some backward compat properties for now
+        self._zapi = self._zapi_wrapper._zapi
+        self._zbx_api_version = self._zapi_wrapper._zbx_api_version
---
+++ plugins/module_utils/wrappers.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+import atexit
+import traceback
+
+from ansible.module_utils.basic import AnsibleModule, missing_required_lib
+from distutils.version import LooseVersion
+
+try:
+    from zabbix_api import ZabbixAPI
+    HAS_ZABBIX_API = True
+except ImportError:
+    ZBX_IMP_ERR = traceback.format_exc()
+    HAS_ZABBIX_API = False
+
+
+class ZapiWrapper(object):
+    """
+    A simple wrapper over the Zabbix API
+    """
+    def __init__(self, module, zbx=None):
+        self._module = module
+
+        if not HAS_ZABBIX_API:
+            module.fail_json(msg=missing_required_lib('zabbix-api', url='https://pypi.org/project/zabbix-api/'), exception=ZBX_IMP_ERR)
+
+        # check if zbx is already instantiated or not
+        if zbx is not None and isinstance(zbx, ZabbixAPI):
+            self._zapi = zbx
+        else:
+            server_url = module.params['server_url']
+            http_login_user = module.params['http_login_user']
+            http_login_password = module.params['http_login_password']
+            validate_certs = module.params['validate_certs']
+            timeout = module.params['timeout']
+            self._zapi = ZabbixAPI(server_url, timeout=timeout, user=http_login_user, passwd=http_login_password,
+                                   validate_certs=validate_certs)
+
+        self.login()
+
+        self._zbx_api_version = self._zapi.api_version()[:5]
+
+    def login(self):
+        # check if api already logged in
+        if not self._zapi.auth != '':
+            try:
+                login_user = self._module.params['login_user']
+                login_password = self._module.params['login_password']
+                self._zapi.login(login_user, login_password)
+                atexit.register(self._zapi.logout)
+            except Exception as e:
+                self._module.fail_json(msg="Failed to connect to Zabbix server: %s" % e)
