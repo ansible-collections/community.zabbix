0dd9a4189f6757519280035aaa960b28090feb9d
Werner Dijkerman <werner@dj-wasabi.nl>
2020-07-29 18:14:36 +0200

    Add Zabbix Server to ci (#150)
    * Added Zabbix Server to CI
    
    * Added the github actions
    
    * Added the paths to the ignore list
    
    * Added missing path to ignore list as mentioned in comment PR
    
    * Correct the filename to ignore
    
    * Updated the Ansible version to 2.9.11
    
    * Set default to correct image;Fixing the pep8 issue for the Invalid escape sequence
    
    Co-authored-by: Werner Dijkerman <werner.dijkerman@iwelcome.com>

---
+++ molecule/zabbix_server/tests/test_default.py
@@ -0,0 +1,62 @@
+import os
+import pytest
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_zabbiserver_running_and_enabled(host):
+    zabbix = host.service("zabbix-server")
+    if host.system_info.distribution == 'centos':
+        assert zabbix.is_enabled
+        assert zabbix.is_running
+    else:
+        assert zabbix.is_running
+
+
+@pytest.mark.parametrize("server", [("zabbix-server-pgsql"), ("zabbix-server-mysql")])
+def test_zabbix_package(host, server):
+    ansible_data = host.ansible.get_variables()
+    zabbixhost = ansible_data['inventory_hostname']
+
+    zabbixhost = zabbixhost.replace("-centos", "")
+    zabbixhost = zabbixhost.replace("-debian", "")
+    zabbixhost = zabbixhost.replace("-ubuntu", "")
+
+    if zabbixhost == server:
+        if host.system_info.distribution in ['debian', 'ubuntu']:
+            zabbix_server = host.package(server)
+            assert zabbix_server.version.startswith("1:5.0")
+        elif host.system_info.distribution == 'centos':
+            zabbix_server = host.package(server)
+            assert zabbix_server.version.startswith("5.0")
+        assert zabbix_server.is_installed
+
+
+def test_zabbix_server_dot_conf(host):
+    zabbix_server_conf = host.file("/etc/zabbix/zabbix_server.conf")
+    assert zabbix_server_conf.user == "zabbix"
+    assert zabbix_server_conf.group == "zabbix"
+    assert zabbix_server_conf.mode == 0o640
+
+    assert zabbix_server_conf.contains("ListenPort=10051")
+    assert zabbix_server_conf.contains("DebugLevel=3")
+
+
+def test_zabbix_include_dir(host):
+    zabbix_include_dir = host.file("/etc/zabbix/zabbix_server.conf.d")
+    assert zabbix_include_dir.is_directory
+    assert zabbix_include_dir.user == "zabbix"
+    assert zabbix_include_dir.group == "zabbix"
+    # assert zabbix_include_dir.mode == 0o644
+
+
+def test_zabbix_server_logfile(host):
+    zabbix_logfile = host.file("/var/log/zabbix/zabbix_server.log")
+
+    assert not zabbix_logfile.contains('Access denied for user')
+    assert not zabbix_logfile.contains('database is down: reconnecting')
+    assert zabbix_logfile.contains('current database version')
+    assert zabbix_logfile.contains(r"server #0 started \[main process\]")
