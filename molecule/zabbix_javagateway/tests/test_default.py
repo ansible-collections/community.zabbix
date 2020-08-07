ab365edaa90e65aa39ee6cdaf64f4610e3cbbfbd
Werner Dijkerman <werner.dijkerman@iwelcome.com>
2020-08-07 12:57:42 +0200

    Make Molecule work for the Zabbix Java Gateway

--- molecule/zabbix_javagateway/tests/test_default.py
+++ molecule/zabbix_javagateway/tests/test_default.py
@@ -6,13 +6,13 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_zabbix_running_and_enabled(Service):
-    zabbix = Service("zabbix-java-gateway")
+def test_zabbix_running_and_enabled(host):
+    zabbix = host.service("zabbix-java-gateway")
     assert zabbix.is_running
 
 
-def test_zabbix_java_gateway_dot_conf(File):
-    zabbix_proxy_conf = File("/etc/zabbix/zabbix_java_gateway.conf")
+def test_zabbix_java_gateway_dot_conf(host):
+    zabbix_proxy_conf = host.file("/etc/zabbix/zabbix_java_gateway.conf")
     assert zabbix_proxy_conf.user == "zabbix"
     assert zabbix_proxy_conf.group == "zabbix"
     assert zabbix_proxy_conf.mode == 0o644
