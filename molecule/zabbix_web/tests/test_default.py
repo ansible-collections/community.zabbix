cc88a49b666482d808946f76eb4761abea49af02
Werner Dijkerman <werner@dj-wasabi.nl>
2020-09-08 08:17:46 +0200

    Made it work so we can also configure/use Nginx for Web (#220)
    * Made it work so we can also configure/use Nginx for the Zabbix Web
    
    Co-authored-by: Werner Dijkerman <werner.dijkerman@iwelcome.com>

--- molecule/zabbix_web/tests/test_default.py
+++ molecule/zabbix_web/tests/test_default.py
@@ -30,13 +30,19 @@ def test_zabbix_package(host, server, redhat, debian):
 
 def test_zabbix_web(host):
     zabbix_web = host.file("/etc/zabbix/web/zabbix.conf.php")
+    ansible_variables = host.ansible.get_variables()
+    zabbix_websrv = str(ansible_variables['zabbix_websrv'])
 
     if host.system_info.distribution in ['debian', 'ubuntu']:
         assert zabbix_web.user == "www-data"
         assert zabbix_web.group == "www-data"
     elif host.system_info.distribution == 'centos':
-        assert zabbix_web.user == "apache"
-        assert zabbix_web.group == "apache"
+        if zabbix_websrv == 'apache':
+            assert zabbix_web.user == "apache"
+            assert zabbix_web.group == "apache"
+        elif zabbix_websrv == 'nginx':
+            assert zabbix_web.user == "nginx"
+            assert zabbix_web.group == "nginx"
     assert zabbix_web.mode == 0o640
 
 
