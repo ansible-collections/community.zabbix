e875f9fa62c4137850e95604a58180d26b064de2
Markus Fischbacher <453368+rockaut@users.noreply.github.com>
2021-12-28 13:10:56 +0100

    Prepare for distutils.version being removed in Python 3.12 (#597)
    * implement distutils workaround
    
    fixes #585
    fixes #596
    
    * fix sanity checks
    
    * add changelogs

--- plugins/modules/zabbix_template.py
+++ plugins/modules/zabbix_template.py
@@ -311,12 +311,13 @@ import traceback
 import re
 import xml.etree.ElementTree as ET
 
-from distutils.version import LooseVersion
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils._text import to_native
 from ansible.module_utils.six import PY2
 
 from ansible_collections.community.zabbix.plugins.module_utils.base import ZabbixBase
+from ansible_collections.community.zabbix.plugins.module_utils.version import LooseVersion
+
 import ansible_collections.community.zabbix.plugins.module_utils.helpers as zabbix_utils
 
 
