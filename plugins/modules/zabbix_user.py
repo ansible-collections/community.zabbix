79d7a0a7547c325e2ca53b717b5e86296db1b009
Nick Wilburn <senior.crepe@gmail.com>
2022-01-16 16:12:19 -0600

    fix: fix incorrect authentication_type query

--- plugins/modules/zabbix_user.py
+++ plugins/modules/zabbix_user.py
@@ -299,12 +299,15 @@ class User(ZabbixBase):
 
     def get_default_authentication(self):
         auth = self._zapi.authentication.get({'output': 'extend'})
-        if auth.authentication_type == 0:
-            return "internal"
-        elif auth.authentication_type == 1:
-            return "LDAP"
-        else:
-            self._module.fail_json(msg="Failed to query authentication type. Unknown authentication type %s" % auth)
+        try:
+            if auth["authentication_type"] == "0":
+                return "internal"
+            elif auth["authentication_type"] == "1":
+                return "LDAP"
+            else:
+                self._module.fail_json(msg="Failed to query authentication type. Unknown authentication type %s" % auth)
+        except Exception as e:
+            self._module.fail_json(msg="Unhandled error while querying authentication type. %s" % (e))
 
     def get_usergroups_by_name(self, usrgrps):
         params = {
