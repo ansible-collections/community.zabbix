b0f09ce44b74d19f33845e032c6e8fdc102ddd04
Evgeny <ey@bgmot.com>
2022-10-05 03:23:19 -0400

    httpapi documentation (#818)
    * Add httpapi connection examples to all modules.
    
    * Fix YAML error.

--- plugins/modules/zabbix_hostmacro.py
+++ plugins/modules/zabbix_hostmacro.py
@@ -66,6 +66,27 @@ extends_documentation_fragment:
 '''
 
 EXAMPLES = r'''
+# Set following variables for Zabbix Server host in play or inventory
+- name: Set connection specific variables
+  set_fact:
+    ansible_network_os: community.zabbix.zabbix
+    ansible_connection: httpapi
+    ansible_httpapi_port: 80
+    ansible_httpapi_use_ssl: false
+    ansible_httpapi_validate_certs: false
+
+# If you want to use Username and Password to be authenticated by Zabbix Server
+- name: Set credentials to access Zabbix Server API
+  set_fact:
+    ansible_user: Admin
+    ansible_httpapi_pass: zabbix
+
+# If you want to use API token to be authenticated by Zabbix Server
+# https://www.zabbix.com/documentation/current/en/manual/web_interface/frontend_sections/administration/general#api-tokens
+- name: Set API token
+  set_fact:
+    ansible_zabbix_auth_key: 8ec0d52432c15c91fcafe9888500cf9a607f44091ab554dbee860f6b44fac895
+
 - name: Create new host macro or update an existing macro's value
   community.zabbix.zabbix_hostmacro:
     host_name: ExampleHost
