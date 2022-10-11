31ccedff3aae7be022d849b8b92ad964e833b50e
Martin van Es <martin@mrvanes.com>
2022-10-11 14:27:38 +0200

    Add deprecation warning

--- plugins/modules/zabbix_user_role.py
+++ plugins/modules/zabbix_user_role.py
@@ -143,6 +143,13 @@ def main():
         supports_check_mode=True
     )
 
+    zabbix_utils.require_creds_params(module)
+
+    for p in ['server_url', 'login_user', 'login_password', 'timeout', 'validate_certs']:
+        if p in module.params:
+            module.warn('Option "%s" is deprecated with the move to httpapi connection '
+                        'and will be removed in the next release' % p)
+
     state = module.params['state']
     name = module.params['name']
     type = zabbix_utils.helper_to_numeric_value(
