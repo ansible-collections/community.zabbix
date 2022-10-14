e1476b231a40e184d729b843f9e5579fd8370f22
Martin van Es <martin@mrvanes.com>
2022-10-14 14:56:30 +0200

    Remove superfluous LooseVersion import

--- plugins/modules/zabbix_user_role.py
+++ plugins/modules/zabbix_user_role.py
@@ -71,7 +71,6 @@ changed:
 '''
 
 from ansible.module_utils.basic import AnsibleModule
-from ansible_collections.community.zabbix.plugins.module_utils.version import LooseVersion
 
 from ansible_collections.community.zabbix.plugins.module_utils.base import ZabbixBase
 import ansible_collections.community.zabbix.plugins.module_utils.helpers as zabbix_utils
