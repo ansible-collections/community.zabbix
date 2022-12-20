0d2dccbd3bf87ad419ca0779162fbaefce1d29f0
Evgeny <ey@bgmot.com>
2022-12-20 05:07:15 -0500

    Fix deprecation warnings. (#863)

--- plugins/modules/zabbix_group.py
+++ plugins/modules/zabbix_group.py
@@ -155,8 +155,8 @@ def main():
 
     zabbix_utils.require_creds_params(module)
 
-    for p in ['server_url', 'login_user', 'login_password', 'timeout', 'validate_certs']:
-        if p in module.params:
+    for p in ['server_url', 'login_user', 'login_password']:
+        if p in module.params and module.params[p] and module.params[p]:
             module.warn('Option "%s" is deprecated with the move to httpapi connection and will be removed in the next release' % p)
 
     host_groups = module.params['host_groups']
