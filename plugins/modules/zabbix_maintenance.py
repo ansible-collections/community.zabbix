f9d8e2653a01bc13b07cb6bce3fe0e2c3505f8c2
Evgeny <ey@bgmot.com>
2023-03-31 07:14:00 -0400

    Convert bool values to lowercase in modules documentation.x (#933)

--- plugins/modules/zabbix_maintenance.py
+++ plugins/modules/zabbix_maintenance.py
@@ -173,7 +173,7 @@ EXAMPLES = r'''
       - www1.example.com
       - db1.example.com
     state: present
-    collect_data: False
+    collect_data: false
 
 - name: Remove maintenance window by name
   # set task level variables as we change ansible_connection plugin here
