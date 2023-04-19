0fe110f872244a4b6cdd406e1fa1201dba7d34cd
Evgeny Yurchenko <ey@bgmot.com>
2023-04-19 08:52:17 -0400

    zabbix_autoregister module cleaned up of old Zabbix versions.

--- plugins/modules/zabbix_autoregister.py
+++ plugins/modules/zabbix_autoregister.py
@@ -8,7 +8,7 @@ from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
 
-DOCUMENTATION = '''
+DOCUMENTATION = """
 ---
 module: zabbix_autoregister
 
@@ -22,7 +22,7 @@ author:
     - ONODERA Masaru(@masa-orca)
 
 requirements:
-    - "python >= 2.6"
+    - "python >= 3.9"
 
 version_added: 1.6.0
 
@@ -47,7 +47,6 @@ options:
         type: str
 
 notes:
-    - Only Zabbix >= 4.4 is supported.
     - This module returns changed=true when any value is set in I(tls_psk_identity) or I(tls_psk) as Zabbix API
       will not return any sensitive information back for module to compare.
     - Please note that this module configures B(global Zabbix Server settings).
@@ -57,9 +56,9 @@ notes:
 extends_documentation_fragment:
 - community.zabbix.zabbix
 
-'''
+"""
 
-EXAMPLES = '''
+EXAMPLES = """
 # If you want to use Username and Password to be authenticated by Zabbix Server
 - name: Set credentials to access Zabbix Server API
   set_fact:
@@ -80,13 +79,13 @@ EXAMPLES = '''
     ansible_httpapi_port: 443
     ansible_httpapi_use_ssl: true
     ansible_httpapi_validate_certs: false
-    ansible_zabbix_url_path: 'zabbixeu'  # If Zabbix WebUI runs on non-default (zabbix) path ,e.g. http://<FQDN>/zabbixeu
+    ansible_zabbix_url_path: "zabbixeu"  # If Zabbix WebUI runs on non-default (zabbix) path ,e.g. http://<FQDN>/zabbixeu
     ansible_host: zabbix-example-fqdn.org
   community.zabbix.zabbix_autoregister:
     tls_accept:
       - unsecure
       - tls_with_psk
-    tls_psk_identity: 'PSK 001'
+    tls_psk_identity: "PSK 001"
     tls_psk: "11111595725ac58dd977beef14b97461a7c1045b9a1c923453302c5473193478"
 
 - name: Set unsecure to tls_accept
@@ -97,39 +96,32 @@ EXAMPLES = '''
     ansible_httpapi_port: 443
     ansible_httpapi_use_ssl: true
     ansible_httpapi_validate_certs: false
-    ansible_zabbix_url_path: 'zabbixeu'  # If Zabbix WebUI runs on non-default (zabbix) path ,e.g. http://<FQDN>/zabbixeu
+    ansible_zabbix_url_path: "zabbixeu"  # If Zabbix WebUI runs on non-default (zabbix) path ,e.g. http://<FQDN>/zabbixeu
     ansible_host: zabbix-example-fqdn.org
   community.zabbix.zabbix_autoregister:
     tls_accept: unsecure
-'''
+"""
 
-RETURN = '''
+RETURN = """ 
 msg:
     description: The result of the operation
     returned: success
     type: str
-    sample: 'Successfully updated global autoregistration setting'
-'''
+    sample: "Successfully updated global autoregistration setting"
+"""
 
 
 from ansible.module_utils.basic import AnsibleModule
-
 from ansible_collections.community.zabbix.plugins.module_utils.base import ZabbixBase
-from ansible.module_utils.compat.version import LooseVersion
-
 import ansible_collections.community.zabbix.plugins.module_utils.helpers as zabbix_utils
 
 
 class Autoregistration(ZabbixBase):
-    def __init__(self, module, zbx=None, zapi_wrapper=None):
-        super(Autoregistration, self).__init__(module, zbx, zapi_wrapper)
-        if LooseVersion(self._zbx_api_version) < LooseVersion('4.4.0'):
-            module.fail_json(msg="This module doesn't support Zabbix versions lower than 4.4.0")
 
     # get autoregistration
     def get_autoregistration(self):
         try:
-            return self._zapi.autoregistration.get({"output": 'extend'})
+            return self._zapi.autoregistration.get({"output": "extend"})
         except Exception as e:
             self._module.fail_json(msg="Failed to get autoregistration: %s" % e)
 
@@ -137,38 +129,38 @@ class Autoregistration(ZabbixBase):
     def update_autoregistration(self, current_setting, tls_accept, tls_psk_identity, tls_psk):
         tls_accept_values = [
             None,
-            'unsecure',
-            'tls_with_psk'
+            "unsecure",
+            "tls_with_psk"
         ]
         params = {}
         try:
             if isinstance(tls_accept, str):
-                params['tls_accept'] = zabbix_utils.helper_to_numeric_value(
+                params["tls_accept"] = zabbix_utils.helper_to_numeric_value(
                     tls_accept_values, tls_accept
                 )
             elif isinstance(tls_accept, list):
-                params['tls_accept'] = 0
+                params["tls_accept"] = 0
                 for _tls_accept_value in tls_accept:
-                    params['tls_accept'] += zabbix_utils.helper_to_numeric_value(
+                    params["tls_accept"] += zabbix_utils.helper_to_numeric_value(
                         tls_accept_values, _tls_accept_value
                     )
             else:
                 self._module.fail_json(msg="Value of tls_accept must be list or string.")
 
             if tls_psk_identity:
-                params['tls_psk_identity'] = tls_psk_identity
+                params["tls_psk_identity"] = tls_psk_identity
 
             if tls_psk:
-                params['tls_psk'] = tls_psk
+                params["tls_psk"] = tls_psk
 
-            current_tls_accept = int(current_setting['tls_accept'])
-            if (current_tls_accept == tls_accept_values.index('unsecure')
-                    and params['tls_accept'] >= tls_accept_values.index('tls_with_psk')):
+            current_tls_accept = int(current_setting["tls_accept"])
+            if (current_tls_accept == tls_accept_values.index("unsecure")
+                    and params["tls_accept"] >= tls_accept_values.index("tls_with_psk")):
                 if not tls_psk_identity or not tls_psk:
                     self._module.fail_json(msg="Please set tls_psk_identity and tls_psk.")
 
             if (not tls_psk_identity and not tls_psk
-                    and params['tls_accept'] == current_tls_accept):
+                    and params["tls_accept"] == current_tls_accept):
                 self._module.exit_json(changed=False, result="Autoregistration is already up to date")
 
             if self._module.check_mode:
@@ -186,12 +178,12 @@ def main():
     argument_spec = zabbix_utils.zabbix_common_argument_spec()
     argument_spec.update(dict(
         tls_accept=dict(
-            type='list',
-            elements='str',
+            type="list",
+            elements="str",
             required=True
         ),
-        tls_psk_identity=dict(type='str', required=False, no_log=True),
-        tls_psk=dict(type='str', required=False, no_log=True),
+        tls_psk_identity=dict(type="str", required=False, no_log=True),
+        tls_psk=dict(type="str", required=False, no_log=True),
     ))
     module = AnsibleModule(
         argument_spec=argument_spec,
@@ -200,14 +192,14 @@ def main():
 
     zabbix_utils.require_creds_params(module)
 
-    tls_accept = module.params['tls_accept']
-    tls_psk_identity = module.params['tls_psk_identity']
-    tls_psk = module.params['tls_psk']
+    tls_accept = module.params["tls_accept"]
+    tls_psk_identity = module.params["tls_psk_identity"]
+    tls_psk = module.params["tls_psk"]
 
     autoregistration_class_obj = Autoregistration(module)
     current_setting = autoregistration_class_obj.get_autoregistration()
     autoregistration_class_obj.update_autoregistration(current_setting, tls_accept, tls_psk_identity, tls_psk)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
