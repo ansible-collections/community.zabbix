0a25f3b3f4a69995947cac1705764621b355d865
ONODERA <masaru-onodera@ieee.org>
2023-04-28 16:02:04 +0100

    Modify description and use httpapi connection plugin

--- plugins/modules/zabbix_regexp.py
+++ plugins/modules/zabbix_regexp.py
@@ -14,17 +14,17 @@ DOCUMENTATION = """
 ---
 module: zabbix_regexp
 
-short_description: Update Zabbix regular expression
+short_description: Create/update/delete Zabbix regular expression
 
 
 description:
-    - This module allows you to modify Zabbix regular expression.
+    - This module allows you to create, update and delete Zabbix regular expression.
 
 author:
     - ONODERA Masaru(@masa-orca)
 
 requirements:
-    - "zabbix-api >= 0.5.4"
+    - "python >= 2.7"
 
 version_added: 1.8.0
 
@@ -91,7 +91,28 @@ extends_documentation_fragment:
 """
 
 EXAMPLES = """
+# If you want to use Username and Password to be authenticated by Zabbix Server
+- name: Set credentials to access Zabbix Server API
+  set_fact:
+    ansible_user: Admin
+    ansible_httpapi_pass: zabbix
+
+# If you want to use API token to be authenticated by Zabbix Server
+# https://www.zabbix.com/documentation/current/en/manual/web_interface/frontend_sections/administration/general#api-tokens
+- name: Set API token
+  set_fact:
+    ansible_zabbix_auth_key: 8ec0d52432c15c91fcafe9888500cf9a607f44091ab554dbee860f6b44fac895
+
 - name: Update regexp of 'File systems for discovery'
+  # set task level variables as we change ansible_connection plugin here
+  vars:
+    ansible_network_os: community.zabbix.zabbix
+    ansible_connection: httpapi
+    ansible_httpapi_port: 443
+    ansible_httpapi_use_ssl: true
+    ansible_httpapi_validate_certs: false
+    ansible_zabbix_url_path: 'zabbixeu'  # If Zabbix WebUI runs on non-default (zabbix) path ,e.g. http://<FQDN>/zabbixeu
+    ansible_host: zabbix-example-fqdn.org
   community.zabbix.zabbix_regexp:
     server_url: "{{ zabbix_api_server_url }}"
     login_user: "{{ zabbix_api_login_user }}"
@@ -302,6 +323,12 @@ def main():
         required_if=[["state", "present", ["expressions"]]],
         supports_check_mode=True,
     )
+
+    zabbix_utils.require_creds_params(module)
+
+    for p in ['server_url', 'login_user', 'login_password', 'timeout', 'validate_certs']:
+        if p in module.params and not module.params[p] is None:
+            module.warn('Option "%s" is deprecated with the move to httpapi connection and will be removed in the next release' % p)
 
     name = module.params["name"]
     test_string = module.params["test_string"]
