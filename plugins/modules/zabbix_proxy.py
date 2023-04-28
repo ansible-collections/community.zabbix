ea90a58ec1abae58005570dc3b5bad0fde11079f
Evgeny Yurchenko <ey@bgmot.com>
2023-04-28 15:48:11 -0400

    zabbix_proxy module cleaned up of old Zabbix versions.

--- plugins/modules/zabbix_proxy.py
+++ plugins/modules/zabbix_proxy.py
@@ -22,7 +22,7 @@ from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 
-DOCUMENTATION = r'''
+DOCUMENTATION = r"""
 ---
 module: zabbix_proxy
 short_description: Create/delete/get/update Zabbix proxies
@@ -31,7 +31,7 @@ description:
 author:
     - "Alen Komic (@akomic)"
 requirements:
-    - "python >= 2.6"
+    - "python >= 3.9"
 options:
     proxy_name:
         description:
@@ -42,7 +42,6 @@ options:
         description:
             - Comma-delimited list of IP/CIDR addresses or DNS names to accept active proxy requests from.
             - Requires I(status=active).
-            - Works only with >= Zabbix 4.0. ( remove option for <= 4.0 )
         required: false
         type: str
     description:
@@ -54,22 +53,22 @@ options:
         description:
             - Type of proxy. (4 - active, 5 - passive)
         required: false
-        choices: ['active', 'passive']
+        choices: ["active", "passive"]
         default: "active"
         type: str
     tls_connect:
         description:
             - Connections to proxy.
         required: false
-        choices: ['no_encryption','PSK','certificate']
-        default: 'no_encryption'
+        choices: ["no_encryption","PSK","certificate"]
+        default: "no_encryption"
         type: str
     tls_accept:
         description:
             - Connections from proxy.
         required: false
-        choices: ['no_encryption','PSK','certificate']
-        default: 'no_encryption'
+        choices: ["no_encryption","PSK","certificate"]
+        default: "no_encryption"
         type: str
     ca_cert:
         description:
@@ -98,14 +97,14 @@ options:
             - On C(present), it will create if proxy does not exist or update the proxy if the associated data is different.
             - On C(absent) will remove a proxy if it exists.
         required: false
-        choices: ['present', 'absent']
+        choices: ["present", "absent"]
         default: "present"
         type: str
     interface:
         description:
             - Dictionary with params for the interface when proxy is in passive mode.
             - For more information, review proxy interface documentation at
-            - U(https://www.zabbix.com/documentation/4.0/manual/api/reference/proxy/object#proxy_interface).
+            - U(https://www.zabbix.com/documentation/current/en/manual/api/reference/proxy/object#proxy-interface).
         required: false
         suboptions:
             useip:
@@ -120,43 +119,27 @@ options:
                 description:
                     - IP address used by proxy interface.
                     - Required if I(useip=1).
-                default: ''
+                default: ""
             dns:
                 type: str
                 description:
                     - DNS name of the proxy interface.
                     - Required if I(useip=0).
-                default: ''
+                default: ""
             port:
                 type: str
                 description:
                     - Port used by proxy interface.
-                default: '10051'
-            type:
-                type: int
-                description:
-                    - Interface type to add.
-                    - This suboption is currently ignored for Zabbix proxy.
-                    - This suboption is deprecated since Ansible 2.10 and will eventually be removed in 2.14.
-                required: false
-                default: 0
-            main:
-                type: int
-                description:
-                    - Whether the interface is used as default.
-                    - This suboption is currently ignored for Zabbix proxy.
-                    - This suboption is deprecated since Ansible 2.10 and will eventually be removed in 2.14.
-                required: false
-                default: 0
+                default: "10051"
         default: {}
         type: dict
 
 extends_documentation_fragment:
 - community.zabbix.zabbix
 
-'''
+"""
 
-EXAMPLES = r'''
+EXAMPLES = r"""
 # If you want to use Username and Password to be authenticated by Zabbix Server
 - name: Set credentials to access Zabbix Server API
   set_fact:
@@ -177,7 +160,7 @@ EXAMPLES = r'''
     ansible_httpapi_port: 443
     ansible_httpapi_use_ssl: true
     ansible_httpapi_validate_certs: false
-    ansible_zabbix_url_path: 'zabbixeu'  # If Zabbix WebUI runs on non-default (zabbix) path ,e.g. http://<FQDN>/zabbixeu
+    ansible_zabbix_url_path: "zabbixeu"  # If Zabbix WebUI runs on non-default (zabbix) path ,e.g. http://<FQDN>/zabbixeu
     ansible_host: zabbix-example-fqdn.org
   community.zabbix.zabbix_proxy:
     proxy_name: ExampleProxy
@@ -194,7 +177,7 @@ EXAMPLES = r'''
     ansible_httpapi_port: 443
     ansible_httpapi_use_ssl: true
     ansible_httpapi_validate_certs: false
-    ansible_zabbix_url_path: 'zabbixeu'  # If Zabbix WebUI runs on non-default (zabbix) path ,e.g. http://<FQDN>/zabbixeu
+    ansible_zabbix_url_path: "zabbixeu"  # If Zabbix WebUI runs on non-default (zabbix) path ,e.g. http://<FQDN>/zabbixeu
     ansible_host: zabbix-example-fqdn.org
   community.zabbix.zabbix_proxy:
     proxy_name: ExampleProxy
@@ -214,7 +197,7 @@ EXAMPLES = r'''
     ansible_httpapi_port: 443
     ansible_httpapi_use_ssl: true
     ansible_httpapi_validate_certs: false
-    ansible_zabbix_url_path: 'zabbixeu'  # If Zabbix WebUI runs on non-default (zabbix) path ,e.g. http://<FQDN>/zabbixeu
+    ansible_zabbix_url_path: "zabbixeu"  # If Zabbix WebUI runs on non-default (zabbix) path ,e.g. http://<FQDN>/zabbixeu
     ansible_host: zabbix-example-fqdn.org
   community.zabbix.zabbix_proxy:
     proxy_name: ExampleProxy
@@ -224,15 +207,14 @@ EXAMPLES = r'''
     interface:
       dns: proxy.example.com
       port: 10051
-'''
+"""
 
-RETURN = r''' # '''
+RETURN = r""" # """
 
 
 from ansible.module_utils.basic import AnsibleModule
 
 from ansible_collections.community.zabbix.plugins.module_utils.base import ZabbixBase
-from ansible.module_utils.compat.version import LooseVersion
 
 import ansible_collections.community.zabbix.plugins.module_utils.helpers as zabbix_utils
 
@@ -243,13 +225,13 @@ class Proxy(ZabbixBase):
         self.existing_data = None
 
     def proxy_exists(self, proxy_name):
-        result = self._zapi.proxy.get({'output': 'extend',
-                                       'selectInterface': 'extend',
-                                       'filter': {'host': proxy_name}})
+        result = self._zapi.proxy.get({"output": "extend",
+                                       "selectInterface": "extend",
+                                       "filter": {"host": proxy_name}})
 
-        if len(result) > 0 and 'proxyid' in result[0]:
+        if len(result) > 0 and "proxyid" in result[0]:
             self.existing_data = result[0]
-            return result[0]['proxyid']
+            return result[0]["proxyid"]
         else:
             return result
 
@@ -263,23 +245,19 @@ class Proxy(ZabbixBase):
                 if data[item]:
                     parameters[item] = data[item]
 
-            if 'proxy_address' in data and data['status'] != '5':
-                parameters.pop('proxy_address', False)
+            if "proxy_address" in data and data["status"] != "5":
+                parameters.pop("proxy_address", False)
 
-            if 'interface' in data and data['status'] != '6':
-                parameters.pop('interface', False)
-            else:
-                if LooseVersion(self._zbx_api_version) >= LooseVersion('6.0'):
-                    parameters['interface'].pop('type')
-                    parameters['interface'].pop('main')
+            if "interface" in data and data["status"] != "6":
+                parameters.pop("interface", False)
 
             proxy_ids_list = self._zapi.proxy.create(parameters)
             self._module.exit_json(changed=True,
-                                   result="Successfully added proxy %s (%s)" % (data['host'], data['status']))
+                                   result="Successfully added proxy %s (%s)" % (data["host"], data["status"]))
             if len(proxy_ids_list) >= 1:
-                return proxy_ids_list['proxyids'][0]
+                return proxy_ids_list["proxyids"][0]
         except Exception as e:
-            self._module.fail_json(msg="Failed to create proxy %s: %s" % (data['host'], e))
+            self._module.fail_json(msg="Failed to create proxy %s: %s" % (data["host"], e))
 
     def delete_proxy(self, proxy_id, proxy_name):
         try:
@@ -299,33 +277,22 @@ class Proxy(ZabbixBase):
             for key in data:
                 if data[key]:
                     parameters[key] = data[key]
-            if 'interface' in parameters:
-                if parameters['status'] == '5':
+            if "interface" in parameters:
+                if parameters["status"] == "5":
                     # Active proxy
-                    parameters.pop('interface', False)
+                    parameters.pop("interface", False)
                 else:
                     # Passive proxy
-                    parameters['interface']['useip'] = str(parameters['interface']['useip'])
-                    if LooseVersion(self._zbx_api_version) >= LooseVersion('6.0.0'):
-                        parameters['interface'].pop('type', False)
-                        parameters['interface'].pop('main', False)
-                    else:
-                        parameters['interface']['type'] = '0'
-                        parameters['interface']['main'] = '1'
-                        if ('interface' in self.existing_data
-                                and isinstance(self.existing_data['interface'], dict)):
-                            new_interface = self.existing_data['interface'].copy()
-                            new_interface.update(parameters['interface'])
-                            parameters['interface'] = new_interface
+                    parameters["interface"]["useip"] = str(parameters["interface"]["useip"])
 
-            if parameters['status'] == '5':
+            if parameters["status"] == "5":
                 # Active proxy
-                parameters.pop('tls_connect', False)
+                parameters.pop("tls_connect", False)
             else:
                 # Passive proxy
-                parameters.pop('tls_accept', False)
+                parameters.pop("tls_accept", False)
 
-            parameters['proxyid'] = proxy_id
+            parameters["proxyid"] = proxy_id
 
             change_parameters = {}
             difference = zabbix_utils.helper_cleanup_data(zabbix_utils.helper_compare_dictionaries(parameters, self.existing_data, change_parameters))
@@ -333,43 +300,41 @@ class Proxy(ZabbixBase):
             if difference == {}:
                 self._module.exit_json(changed=False)
             else:
-                difference['proxyid'] = proxy_id
+                difference["proxyid"] = proxy_id
                 self._zapi.proxy.update(parameters)
                 self._module.exit_json(
                     changed=True,
                     result="Successfully updated proxy %s (%s)" %
-                           (data['host'], proxy_id)
+                           (data["host"], proxy_id)
                 )
         except Exception as e:
             self._module.fail_json(msg="Failed to update proxy %s: %s" %
-                                       (data['host'], e))
+                                       (data["host"], e))
 
 
 def main():
     argument_spec = zabbix_utils.zabbix_common_argument_spec()
     argument_spec.update(dict(
-        proxy_name=dict(type='str', required=True),
-        proxy_address=dict(type='str', required=False),
-        status=dict(type='str', default="active", choices=['active', 'passive']),
-        state=dict(type='str', default="present", choices=['present', 'absent']),
-        description=dict(type='str', required=False),
-        tls_connect=dict(type='str', default='no_encryption', choices=['no_encryption', 'PSK', 'certificate']),
-        tls_accept=dict(type='str', default='no_encryption', choices=['no_encryption', 'PSK', 'certificate']),
-        ca_cert=dict(type='str', required=False, default=None, aliases=['tls_issuer']),
-        tls_subject=dict(type='str', required=False, default=None),
-        tls_psk_identity=dict(type='str', required=False, default=None),
-        tls_psk=dict(type='str', required=False, default=None, no_log=True),
+        proxy_name=dict(type="str", required=True),
+        proxy_address=dict(type="str", required=False),
+        status=dict(type="str", default="active", choices=["active", "passive"]),
+        state=dict(type="str", default="present", choices=["present", "absent"]),
+        description=dict(type="str", required=False),
+        tls_connect=dict(type="str", default="no_encryption", choices=["no_encryption", "PSK", "certificate"]),
+        tls_accept=dict(type="str", default="no_encryption", choices=["no_encryption", "PSK", "certificate"]),
+        ca_cert=dict(type="str", required=False, default=None, aliases=["tls_issuer"]),
+        tls_subject=dict(type="str", required=False, default=None),
+        tls_psk_identity=dict(type="str", required=False, default=None),
+        tls_psk=dict(type="str", required=False, default=None, no_log=True),
         interface=dict(
-            type='dict',
+            type="dict",
             required=False,
             default={},
             options=dict(
-                useip=dict(type='int', choices=[0, 1], default=0),
-                ip=dict(type='str', default=''),
-                dns=dict(type='str', default=''),
-                port=dict(type='str', default='10051'),
-                type=dict(type='int', default=0, removed_in_version="3.0.0", removed_from_collection='community.zabbix'),  # was Ansible 2.14
-                main=dict(type='int', default=0, removed_in_version="3.0.0", removed_from_collection='community.zabbix'),  # was Ansible 2.14
+                useip=dict(type="int", choices=[0, 1], default=0),
+                ip=dict(type="str", default=""),
+                dns=dict(type="str", default=""),
+                port=dict(type="str", default="10051")
             ),
         )
     ))
@@ -380,32 +345,32 @@ def main():
 
     zabbix_utils.require_creds_params(module)
 
-    proxy_name = module.params['proxy_name']
-    proxy_address = module.params['proxy_address']
-    description = module.params['description']
-    status = module.params['status']
-    tls_connect = module.params['tls_connect']
-    tls_accept = module.params['tls_accept']
-    tls_issuer = module.params['ca_cert']
-    tls_subject = module.params['tls_subject']
-    tls_psk_identity = module.params['tls_psk_identity']
-    tls_psk = module.params['tls_psk']
-    state = module.params['state']
-    interface = module.params['interface']
+    proxy_name = module.params["proxy_name"]
+    proxy_address = module.params["proxy_address"]
+    description = module.params["description"]
+    status = module.params["status"]
+    tls_connect = module.params["tls_connect"]
+    tls_accept = module.params["tls_accept"]
+    tls_issuer = module.params["ca_cert"]
+    tls_subject = module.params["tls_subject"]
+    tls_psk_identity = module.params["tls_psk_identity"]
+    tls_psk = module.params["tls_psk"]
+    state = module.params["state"]
+    interface = module.params["interface"]
 
     # convert enabled to 0; disabled to 1
     status = 6 if status == "passive" else 5
 
-    if tls_connect == 'certificate':
+    if tls_connect == "certificate":
         tls_connect = 4
-    elif tls_connect == 'PSK':
+    elif tls_connect == "PSK":
         tls_connect = 2
     else:
         tls_connect = 1
 
-    if tls_accept == 'certificate':
+    if tls_accept == "certificate":
         tls_accept = 4
-    elif tls_accept == 'PSK':
+    elif tls_accept == "PSK":
         tls_accept = 2
     else:
         tls_accept = 1
@@ -421,17 +386,17 @@ def main():
             proxy.delete_proxy(proxy_id, proxy_name)
         else:
             proxy.update_proxy(proxy_id, {
-                'host': proxy_name,
-                'description': description,
-                'status': str(status),
-                'tls_connect': str(tls_connect),
-                'tls_accept': str(tls_accept),
-                'tls_issuer': tls_issuer,
-                'tls_subject': tls_subject,
-                'tls_psk_identity': tls_psk_identity,
-                'tls_psk': tls_psk,
-                'interface': interface,
-                'proxy_address': proxy_address
+                "host": proxy_name,
+                "description": description,
+                "status": str(status),
+                "tls_connect": str(tls_connect),
+                "tls_accept": str(tls_accept),
+                "tls_issuer": tls_issuer,
+                "tls_subject": tls_subject,
+                "tls_psk_identity": tls_psk_identity,
+                "tls_psk": tls_psk,
+                "interface": interface,
+                "proxy_address": proxy_address
             })
     else:
         if state == "absent":
@@ -439,19 +404,19 @@ def main():
             module.exit_json(changed=False)
 
         proxy_id = proxy.add_proxy(data={
-            'host': proxy_name,
-            'description': description,
-            'status': str(status),
-            'tls_connect': str(tls_connect),
-            'tls_accept': str(tls_accept),
-            'tls_issuer': tls_issuer,
-            'tls_subject': tls_subject,
-            'tls_psk_identity': tls_psk_identity,
-            'tls_psk': tls_psk,
-            'interface': interface,
-            'proxy_address': proxy_address
+            "host": proxy_name,
+            "description": description,
+            "status": str(status),
+            "tls_connect": str(tls_connect),
+            "tls_accept": str(tls_accept),
+            "tls_issuer": tls_issuer,
+            "tls_subject": tls_subject,
+            "tls_psk_identity": tls_psk_identity,
+            "tls_psk": tls_psk,
+            "interface": interface,
+            "proxy_address": proxy_address
         })
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
