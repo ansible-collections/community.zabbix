e2bf41e4e4808c076407161b65cddba7331d9a2d
ONODERA <masaru-onodera@ieee.org>
2023-04-28 11:25:54 +0100

    Use helper_compare_lists function

--- plugins/modules/zabbix_regexp.py
+++ plugins/modules/zabbix_regexp.py
@@ -6,10 +6,11 @@
 
 
 from __future__ import absolute_import, division, print_function
+
 __metaclass__ = type
 
 
-DOCUMENTATION = '''
+DOCUMENTATION = """
 ---
 module: zabbix_regexp
 
@@ -87,9 +88,9 @@ notes:
 extends_documentation_fragment:
 - community.zabbix.zabbix
 
-'''
+"""
 
-EXAMPLES = '''
+EXAMPLES = """
 - name: Update regexp of 'File systems for discovery'
   community.zabbix.zabbix_regexp:
     server_url: "{{ zabbix_api_server_url }}"
@@ -100,21 +101,21 @@ EXAMPLES = '''
     expressions:
       - expression: "^(btrfs|ext2|ext3|ext4|reiser|xfs|ffs|ufs|jfs|jfs2|vxfs|hfs|apfs|refs|ntfs|fat32|zfs)$"
         expression_type: result_is_true
-'''
+"""
 
-RETURN = '''
+RETURN = """
 msg:
     description: The result of the operation
     returned: success
     type: str
     sample: 'Successfully updated regular expression setting'
-'''
+"""
 
 
 from ansible.module_utils.basic import AnsibleModule
 
 from ansible_collections.community.zabbix.plugins.module_utils.base import ZabbixBase
-from ansible_collections.community.zabbix.plugins.module_utils.version import LooseVersion
+from ansible.module_utils.compat.version import LooseVersion
 
 import ansible_collections.community.zabbix.plugins.module_utils.helpers as zabbix_utils
 
@@ -122,8 +123,10 @@ import ansible_collections.community.zabbix.plugins.module_utils.helpers as zabb
 class RegularExpression(ZabbixBase):
     def __init__(self, module, zbx=None, zapi_wrapper=None):
         super(RegularExpression, self).__init__(module, zbx, zapi_wrapper)
-        if LooseVersion(self._zbx_api_version) < LooseVersion('6.0'):
-            module.fail_json(msg="This module doesn't support Zabbix versions lower than 6.0")
+        if LooseVersion(self._zbx_api_version) < LooseVersion("6.0"):
+            module.fail_json(
+                msg="This module doesn't support Zabbix versions lower than 6.0"
+            )
 
     def get_regexps(self, regexp_name):
         try:
@@ -134,79 +137,64 @@ class RegularExpression(ZabbixBase):
                         "expression",
                         "expression_type",
                         "exp_delimiter",
-                        "case_sensitive"
+                        "case_sensitive",
                     ],
-                    "filter": {
-                        'name': regexp_name
-                    }
+                    "filter": {"name": regexp_name},
                 }
             )
             if len(regexps) >= 2:
                 self._module.fail_json("Too many regexps are matched.")
             return regexps
         except Exception as e:
-            self._module.fail_json(msg="Failed to get regular expression setting: %s" % e)
+            self._module.fail_json(
+                msg="Failed to get regular expression setting: %s" % e
+            )
 
     def delete_regexp(self, regexp):
         try:
-            parameter = [regexp['regexpid']]
+            parameter = [regexp["regexpid"]]
             if self._module.check_mode:
                 self._module.exit_json(changed=True)
             self._zapi.regexp.delete(parameter)
-            self._module.exit_json(changed=True, msg="Successfully deleted regular expression setting.")
+            self._module.exit_json(
+                changed=True, msg="Successfully deleted regular expression setting."
+            )
         except Exception as e:
-            self._module.fail_json(msg="Failed to delete regular expression setting: %s" % e)
-
-    def _compare_expressions(self, current_expressions, future_expressions):
-        if len(current_expressions) != len(future_expressions):
-            return False
-        for index in range(len(current_expressions)):
-            current_expression = current_expressions[index]
-            future_expression = future_expressions[index]
-
-            if(current_expression['expression'] != future_expression['expression']):
-                return False
-
-            if(current_expression['expression_type'] != future_expression['expression_type']):
-                return False
-
-            if(current_expression['expression_type'] == '1'
-                    and current_expression['exp_delimiter'] != future_expression['exp_delimiter']):
-                return False
-
-            if(current_expression['case_sensitive'] != future_expression['case_sensitive']):
-                return False
-
-        return True
+            self._module.fail_json(
+                msg="Failed to delete regular expression setting: %s" % e
+            )
 
     def _convert_expressions_to_json(self, expressions):
         expression_type_values = [
-            'character_string_included',
-            'any_character_string_included',
-            'character_string_not_included',
-            'result_is_true',
-            'result_is_false'
+            "character_string_included",
+            "any_character_string_included",
+            "character_string_not_included",
+            "result_is_true",
+            "result_is_false",
         ]
 
         expression_jsons = []
         for expression in expressions:
             expression_json = {}
 
-            expression_json['expression'] = expression['expression']
+            expression_json["expression"] = expression["expression"]
             expression_type = zabbix_utils.helper_to_numeric_value(
-                expression_type_values,
-                expression['expression_type']
+                expression_type_values, expression["expression_type"]
             )
-            expression_json['expression_type'] = str(expression_type)
-            if expression['exp_delimiter']:
-                if expression['expression_type'] == 'any_character_string_included':
-                    expression_json['exp_delimiter'] = expression['exp_delimiter']
+            expression_json["expression_type"] = str(expression_type)
+            if expression["expression_type"] == "any_character_string_included":
+                if expression["exp_delimiter"]:
+                    expression_json["exp_delimiter"] = expression["exp_delimiter"]
                 else:
-                    self._module.warn("A value of exp_delimiter will be ignored because expression_type is not 'any_character_string_included'.")
-            case_sensitive = '0'
-            if expression['case_sensitive']:
-                case_sensitive = '1'
-            expression_json['case_sensitive'] = case_sensitive
+                    expression_json["exp_delimiter"] = ","
+            elif expression["exp_delimiter"]:
+                self._module.warn(
+                    "A value of exp_delimiter will be ignored because expression_type is not 'any_character_string_included'."
+                )
+            case_sensitive = "0"
+            if expression["case_sensitive"]:
+                case_sensitive = "1"
+            expression_json["case_sensitive"] = case_sensitive
 
             expression_jsons.append(expression_json)
         return expression_jsons
@@ -215,106 +203,115 @@ class RegularExpression(ZabbixBase):
         try:
             if self._module.check_mode:
                 self._module.exit_json(changed=True)
-            self._zapi.regexp.create({
-                'name': name,
-                'test_string': test_string,
-                'expressions': self._convert_expressions_to_json(expressions)
-            })
-            self._module.exit_json(changed=True, msg="Successfully created regular expression setting.")
+            self._zapi.regexp.create(
+                {
+                    "name": name,
+                    "test_string": test_string,
+                    "expressions": self._convert_expressions_to_json(expressions),
+                }
+            )
+            self._module.exit_json(
+                changed=True, msg="Successfully created regular expression setting."
+            )
         except Exception as e:
-            self._module.fail_json(msg="Failed to create regular expression setting: %s" % e)
+            self._module.fail_json(
+                msg="Failed to create regular expression setting: %s" % e
+            )
 
     def update_regexp(self, current_regexp, name, test_string, expressions):
         try:
-            current_expressions = sorted(current_regexp['expressions'], key=lambda x: x['expression'])
+            current_expressions = []
+            for expression in current_regexp["expressions"]:
+                if expression["expression_type"] != "1":
+                    expression = zabbix_utils.helper_normalize_data(
+                        expression, del_keys=["exp_delimiter"]
+                    )[0]
+                current_expressions.append(expression)
             future_expressions = self._convert_expressions_to_json(expressions)
-            future_expressions = sorted(future_expressions, key=lambda x: x['expression'])
-            if (current_regexp['name'] == name
-                    and current_regexp['test_string'] == test_string
-                    and self._compare_expressions(current_expressions, future_expressions)):
+            diff_expressions = []
+            zabbix_utils.helper_compare_lists(
+                current_expressions, future_expressions, diff_expressions
+            )
+            if (
+                current_regexp["name"] == name
+                and current_regexp["test_string"] == test_string
+                and len(diff_expressions) == 0
+            ):
                 self._module.exit_json(changed=False)
             else:
                 if self._module.check_mode:
                     self._module.exit_json(changed=True)
-                self._zapi.regexp.update({
-                    'regexpid': current_regexp['regexpid'],
-                    'name': name,
-                    'test_string': test_string,
-                    'expressions': future_expressions
-                })
-                self._module.exit_json(changed=True, msg="Successfully updated regular expression setting.")
+                self._zapi.regexp.update(
+                    {
+                        "regexpid": current_regexp["regexpid"],
+                        "name": name,
+                        "test_string": test_string,
+                        "expressions": future_expressions,
+                    }
+                )
+                self._module.exit_json(
+                    changed=True, msg="Successfully updated regular expression setting."
+                )
         except Exception as e:
-            self._module.fail_json(msg="Failed to update regular expression setting: %s" % e)
+            self._module.fail_json(
+                msg="Failed to update regular expression setting: %s" % e
+            )
 
 
 def main():
-    """Main ansible module function
-    """
+    """Main ansible module function"""
 
     argument_spec = zabbix_utils.zabbix_common_argument_spec()
-    argument_spec.update(dict(
-        name=dict(
-            type='str',
-            required=True
-        ),
-        test_string=dict(
-            type='str',
-        ),
-        expressions=dict(
-            type='list',
-            elements='dict',
-            options=dict(
-                expression=dict(
-                    type='str',
-                    required=True
+    argument_spec.update(
+        dict(
+            name=dict(type="str", required=True),
+            test_string=dict(
+                type="str",
+            ),
+            expressions=dict(
+                type="list",
+                elements="dict",
+                options=dict(
+                    expression=dict(type="str", required=True),
+                    expression_type=dict(
+                        type="str",
+                        required=True,
+                        choices=[
+                            "character_string_included",
+                            "any_character_string_included",
+                            "character_string_not_included",
+                            "result_is_true",
+                            "result_is_false",
+                        ],
+                    ),
+                    exp_delimiter=dict(type="str", choices=[",", ".", "/"]),
+                    case_sensitive=dict(type="bool", default=False),
                 ),
-                expression_type=dict(
-                    type='str',
-                    required=True,
-                    choices=[
-                        "character_string_included",
-                        "any_character_string_included",
-                        "character_string_not_included",
-                        "result_is_true",
-                        "result_is_false"
-                    ]
-                ),
-                exp_delimiter=dict(
-                    type='str',
-                    choices=[",", ".", "/"]
-                ),
-                case_sensitive=dict(
-                    type='bool',
-                    default=False
-                )
-            )
-        ),
-        state=dict(
-            type='str',
-            required=False,
-            default='present',
-            choices=['present', 'absent'])
-    ))
+            ),
+            state=dict(
+                type="str",
+                required=False,
+                default="present",
+                choices=["present", "absent"],
+            ),
+        )
+    )
 
     module = AnsibleModule(
         argument_spec=argument_spec,
-        required_if=[
-            ['state', 'present', [
-                'expressions'
-            ]]
-        ],
-        supports_check_mode=True
+        required_if=[["state", "present", ["expressions"]]],
+        supports_check_mode=True,
     )
 
-    name = module.params['name']
-    test_string = module.params['test_string']
-    expressions = module.params['expressions']
-    state = module.params['state']
+    name = module.params["name"]
+    test_string = module.params["test_string"]
+    expressions = module.params["expressions"]
+    state = module.params["state"]
 
     regexp_class_obj = RegularExpression(module)
     regexps = regexp_class_obj.get_regexps(name)
 
-    if state == 'absent':
+    if state == "absent":
         if len(regexps) == 1:
             regexp_class_obj.delete_regexp(regexps[0])
         else:
@@ -326,5 +323,5 @@ def main():
             regexp_class_obj.create_regexp(name, test_string, expressions)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
