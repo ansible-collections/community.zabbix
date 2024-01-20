fd394fc26d3228a2688f061bbfc4b39ee0619d78
Troy W <pyrodie18@gmail.com>
2024-01-20 00:44:16 -0500

    Updated Template Group Documentation

--- plugins/modules/zabbix_templategroup.py
+++ plugins/modules/zabbix_templategroup.py
@@ -67,7 +67,7 @@ EXAMPLES = r'''
     ansible_httpapi_validate_certs: false
     ansible_zabbix_url_path: 'zabbixeu'  # If Zabbix WebUI runs on non-default (zabbix) path ,e.g. http://<FQDN>/zabbixeu
     ansible_host: zabbix-example-fqdn.org
-  community.zabbix.zabbix_group:
+  community.zabbix.zabbix_templategroup:
     state: present
     template_groups:
       - Example group1
@@ -84,7 +84,7 @@ EXAMPLES = r'''
       ansible_httpapi_validate_certs: false
       ansible_zabbix_url_path: 'zabbixeu'  # If Zabbix WebUI runs on non-default (zabbix) path ,e.g. http://<FQDN>/zabbixeu
       ansible_host: zabbix-example-fqdn.org
-  community.zabbix.zabbix_group:
+  community.zabbix.zabbix_templategroup:
     state: present
     template_groups:
       - Example group1
