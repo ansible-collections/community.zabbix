f86bdbab47c541ce46818847ee275739bc47c6a9
Andrew Lathrop <alathrop@zabbix-test.usiad51.as12008.net>
2024-05-30 20:34:02 +0000

    add support for tags

--- plugins/modules/zabbix_host_events_info.py
+++ plugins/modules/zabbix_host_events_info.py
@@ -104,6 +104,9 @@ triggers_problem:
                 eventid:
                     description: ID of the event
                     type: int
+                tags:
+                    description: List of tags
+                    type: list
                 value:
                     description: State of the related object
                     type: int
@@ -177,6 +180,35 @@ options:
             - high
             - disaster
         type: str
+    tags:
+        description:
+            - list of tags to filter by
+        required: false
+        type: list
+        suboptions:
+          tag:
+            description:
+                - the tag name
+            required: true
+            type: str
+          value:
+            description:
+                - the tag value
+            required: true
+            type: str
+          operator:
+            description:
+                - how to match tags
+            required: true
+            type: str
+            choices:
+                - like
+                - equal
+                - not_like
+                - not_equal
+                - exists
+                - not_exists
+
 extends_documentation_fragment:
 - community.zabbix.zabbix
 
@@ -233,11 +265,15 @@ class Host(ZabbixBase):
         else:
             return host[0]
 
-    def get_triggers_by_host_id_in_problem_state(self, host_id, trigger_severity):
+    def get_triggers_by_host_id_in_problem_state(self, host_id, trigger_severity, tags=None):
         """ Get triggers in problem state from a hostid"""
         output = "extend"
-        triggers_list = self._zapi.trigger.get({"output": output, "hostids": host_id,
-                                                "min_severity": trigger_severity})
+        if tags:
+            triggers_list = self._zapi.trigger.get({"output": output, "hostids": host_id,
+                                                    "min_severity": trigger_severity, "tags": tags})
+        else:
+            triggers_list = self._zapi.trigger.get({"output": output, "hostids": host_id,
+                                                    "min_severity": trigger_severity})
         return triggers_list
 
     def get_last_event_by_trigger_id(self, triggers_id):
@@ -245,7 +281,7 @@ class Host(ZabbixBase):
         output = ["eventid", "clock", "acknowledged", "value"]
         select_acknowledges = ["clock", "alias", "message"]
         event = self._zapi.event.get({"output": output, "objectids": triggers_id,
-                                      "select_acknowledges": select_acknowledges, "limit": 1, "sortfield": "clock",
+                                      "select_acknowledges": select_acknowledges, "selectTags": "extend", "limit": 1, "sortfield": "clock",
                                       "sortorder": "DESC"})
         return event[0]
 
@@ -263,6 +299,10 @@ def main():
             required=False,
             default="average",
             choices=["not_classified", "information", "warning", "average", "high", "disaster"]),
+        tags=dict(
+            type="list",
+            required=False,
+            elements="dict"),
     ))
     module = AnsibleModule(
         argument_spec=argument_spec,
@@ -270,9 +310,11 @@ def main():
     )
 
     trigger_severity_map = {"not_classified": 0, "information": 1, "warning": 2, "average": 3, "high": 4, "disaster": 5}
+    tags_operator_map  = {"like": 0, "equal": 1, "not_like": 2, "not_equal": 3, "exists": 4, "not_exists": 5}
     host_id = module.params["host_identifier"]
     host_id_type = module.params["host_id_type"]
     trigger_severity = trigger_severity_map[module.params["trigger_severity"]]
+    tags = module.params["tags"]
 
     host_inventory = "hostid"
 
@@ -290,7 +332,11 @@ def main():
         # check hostid exist
         zabbix_host = host.get_host(host_id, host_inventory, "hostid")
 
-    triggers = host.get_triggers_by_host_id_in_problem_state(host_id, trigger_severity)
+    if tags:
+        for tag in tags:
+            tag["operator"] = tags_operator_map[tag["operator"]]
+
+    triggers = host.get_triggers_by_host_id_in_problem_state(host_id, trigger_severity, tags)
 
     triggers_ok = []
     triggers_problem = []
