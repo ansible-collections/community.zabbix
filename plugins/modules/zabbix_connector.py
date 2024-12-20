ca168b94a80b3f1f50beb46fc92824db73baff5b
loricvdt <loric.vandentempel@spaceapplications.com>
2024-12-20 12:38:16 +0100

    Add documentation to zabbix_connector module

--- plugins/modules/zabbix_connector.py
+++ plugins/modules/zabbix_connector.py
@@ -6,14 +6,227 @@
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
-# TODO
 DOCUMENTATION = r"""
+---
+module: zabbix_connector
+short_description: Create/Delete/Update Zabbix connectors
+description:
+    - This module allows you to create, modify and delete Zabbix connectors.
+author:
+    - Loric Vandentempel (@loricvdt)
+requirements:
+    - "python >= 3.9"
+version_added: 3.3.0
+options:
+    name:
+        description:
+            - Name of the connector.
+        type: str
+        required: true
+    url:
+        description:
+            - URL of the receiver used by the connector.
+        type: str
+    data_type:
+        description:
+            - Type of data being streamed.
+        type: str
+        choices: ["item_values", "events"]
+        default: item_values
+    item_value_types:
+        description:
+            - List of value types to stream.
+            - Each element should be one of C(all), C(all+bin), C(numeric_float), C(character), C(log), C(numeric_unsigned), C(text) or C(binary).
+            - C(all) represents all types except binary I((default)).
+            - C(all+bin) represents all types, binary included.
+        type: list
+        elements: str
+        default: ["all"]
+    max_records:
+        description:
+            - Maximum number of records sent in one message.
+        type: int
+    max_senders:
+        description:
+            - Number of sender processes used by the connector.
+        type: int
+    max_attempts:
+        description:
+            - Maximum number of attempts.
+        type: int
+    attempt_interval:
+        description:
+            - Interval between attempts in seconds (value between 0 and 10).
+        type: int
+    timeout:
+        description:
+            - Message sending timeout in seconds (value between 1 and 60).
+        type: int
+    http_proxy:
+        description:
+            - HTTP(S) proxy used by the connector.
+        type: str
+    auth_type:
+        description:
+            - HTTP authentication method used by the connector.
+        type: str
+        choices: ["none", "basic", "ntlm", "kerberos", "digest", "bearer"]
+        default: none
+    username:
+        description:
+            - Username to authenticate the connector with the receiver.
+            - Supported only if C(auth_type) is one of C(basic), C(ntlm), C(kerberos) or C(digest).
+        type: str
+    password:
+        description:
+            - Password to authenticate the connector with the receiver.
+            - Supported only if C(auth_type) is one of C(basic), C(ntlm), C(kerberos) or C(digest).
+        type: str
+    token:
+        description:
+            - Bearer token to authenticate the connector with the receiver.
+            - Required when C(auth_type=bearer).
+        type: str
+    verify_peer:
+        description:
+            - Whether the connector should verify the receiver's certificate.
+        type: bool
+    verify_host:
+        description:
+            - Whether the connector should verify the receiver's hostname against the certificate's CN or SAN fields.
+        type: bool
+    ssl_cert_file:
+        description:
+            - Public SSL key file path for client authentication.
+        type: str
+    ssl_key_file:
+        description:
+            - Private SSL key file path for client authentication.
+        type: str
+    ssl_key_password:
+        description:
+            - Password of the private SSL key file.
+        type: str
+    description:
+        description:
+            - Description of the connector.
+        type: str
+    enabled:
+        description:
+            - Whether the connector is enabled.
+            - (mapped to the I(status) property of the connector object in the Zabbix API)
+        type: bool
+    tags_eval_type:
+        description:
+            - Tag filter evaluation method.
+        type: str
+        choices: ["and/or", "or"]
+        default: and/or
+    tags:
+        description:
+            - List of tags to filter streamed data.
+        type: list
+        elements: dict
+        suboptions:
+            tag:
+                description:
+                    - Name of the tag.
+                type: str
+                required: true
+            operator:
+                description:
+                    - Conditional operator used to filter .
+                type: str
+                choices: ["equals", "does not equal", "contains", "does not contain", "exists", "does not exist"]
+                default: equals
+            value:
+                description:
+                    - Value of the tag compared.
+                type: str
+    state:
+        description:
+            - State of the connector.
+            - On C(present), it will create a connector if it does not exist or update the connector if the associated data is different.
+            - On C(absent), it will remove the connector if it exists.
+        type: str
+        choices: ["present", "absent"]
+        default: present
+
+extends_documentation_fragment:
+    - community.zabbix.zabbix
 """
 
 EXAMPLES = r"""
+# If you want to use Username and Password to be authenticated by Zabbix Server
+- name: Set credentials to access Zabbix Server API
+  ansible.builtin.set_fact:
+    ansible_user: Admin
+    ansible_httpapi_pass: zabbix
+
+# If you want to use API token to be authenticated by Zabbix Server
+# https://www.zabbix.com/documentation/current/en/manual/web_interface/frontend_sections/administration/general#api-tokens
+- name: Set API token
+  ansible.builtin.set_fact:
+    ansible_zabbix_auth_key: 8ec0d52432c15c91fcafe9888500cf9a607f44091ab554dbee860f6b44fac895
+
+- name: Create or update a connector
+  # set task level variables as we change ansible_connection plugin here
+  vars:
+    ansible_network_os: community.zabbix.zabbix
+    ansible_connection: httpapi
+    ansible_httpapi_port: 443
+    ansible_httpapi_use_ssl: true
+    ansible_httpapi_validate_certs: false
+    ansible_zabbix_url_path: "zabbixeu"  # If Zabbix WebUI runs on non-default (zabbix) path ,e.g. http://<FQDN>/zabbixeu
+    ansible_host: zabbix-example-fqdn.org
+  community.zabbix.zabbix_proxy:
+    name: My app connector
+    description: Connector to send item values to My app
+    state: present
+    enabled: true
+    data_type: item_values
+    url: https://my-app.example.com/api/zabbix-stream
+    auth_type: bearer
+    token: "{{ my_app_bearer_token }}"
+    value_types:
+      - numeric_float
+      - character
+      - numeric_unsigned
+      - text
+    tags_eval_type: and/or
+    tags:
+      - tag: to-my-app
+        operator: exists
+      - tag: privacy
+        operator: does not equal
+        value: hidden
+        
+- name: Delete a connector
+  # set task level variables as we change ansible_connection plugin here
+  vars:
+    ansible_network_os: community.zabbix.zabbix
+    ansible_connection: httpapi
+    ansible_httpapi_port: 443
+    ansible_httpapi_use_ssl: true
+    ansible_httpapi_validate_certs: false
+    ansible_zabbix_url_path: "zabbixeu"  # If Zabbix WebUI runs on non-default (zabbix) path ,e.g. http://<FQDN>/zabbixeu
+    ansible_host: zabbix-example-fqdn.org
+  community.zabbix.zabbix_proxy:
+    name: My app connector
+    state: absent
 """
 
 RETURN = r"""
+msg:
+    description: Text result of the operation.
+    returned: always
+    type: str
+    sample: Successfully updated connector 'My app connector' (id: 2)
+result:
+    description: JSON result of the Zabbix API call
+    returned: success
+    type: dict
+    sample: {"connectorids": ["2"]}
 """
 
 from ansible.module_utils.basic import AnsibleModule
@@ -63,6 +276,11 @@ class Connector(ZabbixBase):
     }
 
     def get_connector(self, name):
+        """
+        Gets the connector for the given name and returns its id, or None if it doesn't exist
+        :param name: name of the connector
+        :return: connector ID
+        """
         try:
             result = self._zapi.connector.get({
                 "filter": {"name": name},
@@ -75,38 +293,58 @@ class Connector(ZabbixBase):
             else:
                 return None
         except Exception as e:
-            self._module.fail_json(msg="Failed to get connector %s: %s" % (name, e))
+            self._module.fail_json(msg="Failed to get connector '%s': %s" % (name, e))
 
     def add_connector(self, params):
+        """
+        Creates the connector with the given parameters
+        :param params: parameter dictionary (see Zabbix API)
+        """
         try:
             result = self._zapi.connector.create(params)
-            self._module.exit_json(changed=True, msg="Successfully created connector %s" % params["name"], result=result)
+            self._module.exit_json(changed=True, msg="Successfully created connector '%s'" % params["name"], result=result)
         except Exception as e:
-            self._module.fail_json(msg="Failed to create connector %s: %s" % (params["name"], e))
+            self._module.fail_json(msg="Failed to create connector '%s': %s" % (params["name"], e))
 
     def update_connector(self, connector_id, params):
+        """
+        Updates the connector with the given ID with the provided updated parameters, only if they lead to changes
+        :param connector_id: ID of the connector to update
+        :param params: updated parameters
+        """
         diff = {
             k: v for k, v in params.items() if self.existing_data.get(k) != v
         }
         if diff == {}:
-            self._module.exit_json(changed=False, msg="Connector %s (%s) already up to date" % (params["name"], connector_id))
+            self._module.exit_json(
+                changed=False, msg="Connector '%s' (id: %s) already up to date" % (params["name"], connector_id), result={"connectorids": [str(connector_id)]})
         else:
             try:
                 diff["connectorid"] = connector_id
                 result = self._zapi.connector.update(diff)
                 self._module.exit_json(
-                    changed=True, msg="Successfully updated connector %s (%s)" % (params["name"], connector_id), result=result)
+                    changed=True, msg="Successfully updated connector '%s' (id: %s)" % (params["name"], connector_id), result=result)
             except Exception as e:
-                self._module.fail_json(msg="Failed to update connector %s: %s" % (params["name"], e))
+                self._module.fail_json(msg="Failed to update connector '%s': %s" % (params["name"], e))
 
     def delete_connector(self, connector_id, name):
+        """
+        Deletes the connector with the given ID
+        :param connector_id: ID of the connector to delete
+        :param name: name opf the connector (only used for verbosity)
+        """
         try:
             result = self._zapi.connector.delete([connector_id])
-            self._module.exit_json(changed=True, msg="Successfully deleted connector %s (%s)" % (name, connector_id), result=result)
+            self._module.exit_json(changed=True, msg="Successfully deleted connector '%s' (id: %s)" % (name, connector_id), result=result)
         except Exception as e:
-            self._module.fail_json(msg="Failed to delete connector %s: %s" % (name, e))
+            self._module.fail_json(msg="Failed to delete connector '%s': %s" % (name, e))
 
     def sanitize_params(self, params):
+        """
+        Transforms the module parameters to their corresponding Zabbix API values
+        :param params: module parameters
+        :return: Zabbix API compatible parameters
+        """
         sanitized = {
             "name": params["name"],
             "url": params["url"],
@@ -114,8 +352,8 @@ class Connector(ZabbixBase):
             "max_records": str(params["max_records"]) if params["max_records"] is not None else None,
             "max_senders": str(params["max_senders"]) if params["max_senders"] is not None else None,
             "max_attempts": str(params["max_attempts"]) if params["max_attempts"] is not None else None,
-            "attempt_interval": params["attempt_interval"],
-            "timeout": params["timeout"],
+            "attempt_interval": str(params["attempt_interval"]) if params["attempt_interval"] is not None else None,
+            "timeout": str(params["timeout"]) if params["timeout"] is not None else None,
             "http_proxy": params["http_proxy"],
             "auth_type": Connector.AUTH_TYPES.get(params["auth_type"]),
             "username": params["username"],
@@ -132,7 +370,7 @@ class Connector(ZabbixBase):
         }
 
         if params["item_value_types"] is not None:
-            value_types = params["item_value_types"]
+            value_types = set(params["item_value_types"])
             if "all" in value_types:
                 sanitized["item_value_type"] = "31"
             elif "all+bin" in value_types:
@@ -162,6 +400,9 @@ class Connector(ZabbixBase):
 
 
 def main():
+    """
+    Main module entry point
+    """
     argument_spec = zabbix_utils.zabbix_common_argument_spec()
     argument_spec.update(dict(
         name=dict(type="str", required=True),
@@ -171,8 +412,8 @@ def main():
         max_records=dict(type="int"),
         max_senders=dict(type="int"),
         max_attempts=dict(type="int"),
-        attempt_interval=dict(type="str"),
-        timeout=dict(type="str"),
+        attempt_interval=dict(type="int"),
+        timeout=dict(type="int"),
         http_proxy=dict(type="str"),
         auth_type=dict(type="str", choices=Connector.AUTH_TYPES.keys()),
         username=dict(type="str"),
@@ -223,7 +464,7 @@ def main():
         if connector_id:
             connector.delete_connector(connector_id, name)
         else:
-            module.exit_json(changed=False, msg="Connector %s already absent" % name)
+            module.exit_json(changed=False, msg="Connector '%s' already absent" % name)
     elif state == "present":
         params = connector.sanitize_params(module.params)
 
