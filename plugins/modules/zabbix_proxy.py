fa7cff6ac077c92a6b87b5fbf19f5b8026174054
Evgeny Yurchenko <ey@bgmot.com>
2025-02-17 18:10:38 -0500

    zabbix_proxy module: address should be 127.0.0.1 when operating_mode is active.

--- plugins/modules/zabbix_proxy.py
+++ plugins/modules/zabbix_proxy.py
@@ -674,7 +674,7 @@ def main():
     else:
         allowed_addresses = module.params["allowed_addresses"]
         operating_mode = 1 if module.params["operating_mode"] == "passive" else 0
-        address = module.params["address"]
+        address = '127.0.0.1' if operating_mode == 0 else module.params["address"]
         port = module.params["port"]
         proxy_group = module.params["proxy_group"]
         local_address = module.params["local_address"]
