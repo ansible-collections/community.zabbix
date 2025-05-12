25567785485996fa752ba27c12bed7077b9e2f72
ONODERA Masaru <46081939+masa-orca@users.noreply.github.com>
2025-05-12 09:09:10 +0900

    Add propagate parameter to template group module and host group module (#1526)
    * Add propagate parameter
    
    * Add changelog
    
    * add example

--- plugins/modules/zabbix_templategroup.py
+++ plugins/modules/zabbix_templategroup.py
@@ -4,10 +4,11 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
+
 __metaclass__ = type
 
 
-DOCUMENTATION = r'''
+DOCUMENTATION = r"""
 ---
 module: zabbix_templategroup
 short_description: Create/delete Zabbix template groups
@@ -35,15 +36,26 @@ options:
         type: list
         elements: str
         aliases: [ "template_group" ]
-
+    propagate:
+        description:
+            - List of settings will be propagated.
+            - This module propagates permissions after creating missing template groups.
+            - This parameter is for Zabbix >= 6.2.
+        type: dict
+        suboptions:
+            permissions:
+                description:
+                    - If set C(true), permissions will be propagated.
+                type: bool
+                default: false
 extends_documentation_fragment:
 - community.zabbix.zabbix
 
 notes:
     - Too many concurrent updates to the same group may cause Zabbix to return errors, see examples for a workaround if needed.
-'''
+"""
 
-EXAMPLES = r'''
+EXAMPLES = r"""
 # If you want to use Username and Password to be authenticated by Zabbix Server
 - name: Set credentials to access Zabbix Server API
   ansible.builtin.set_fact:
@@ -90,10 +102,28 @@ EXAMPLES = r'''
       - Example group1
       - Example group2
   when: inventory_hostname==groups['group_name'][0]
-'''
+
+- name: Propagate permissions to sub template group
+  vars:
+    ansible_network_os: community.zabbix.zabbix
+    ansible_connection: httpapi
+    ansible_httpapi_port: 443
+    ansible_httpapi_use_ssl: true
+    ansible_httpapi_validate_certs: false
+    ansible_zabbix_url_path: 'zabbixeu'
+    ansible_host: zabbix-example-fqdn.org
+  community.zabbix.zabbix_templategroup:
+    state: present
+    template_groups:
+      - Example group1
+    propagate:
+      permissions: true
+"""
 
 
 from ansible.module_utils.basic import AnsibleModule
+
+from ansible.module_utils.compat.version import LooseVersion
 
 from ansible_collections.community.zabbix.plugins.module_utils.base import ZabbixBase
 import ansible_collections.community.zabbix.plugins.module_utils.helpers as zabbix_utils
@@ -105,11 +135,11 @@ class TemplateGroup(ZabbixBase):
         try:
             group_add_list = []
             for group_name in group_names:
-                result = self._zapi.templategroup.get({'filter': {'name': group_name}})
+                result = self._zapi.templategroup.get({"filter": {"name": group_name}})
                 if not result:
                     if self._module.check_mode:
                         self._module.exit_json(changed=True)
-                    self._zapi.templategroup.create({'name': group_name})
+                    self._zapi.templategroup.create({"name": group_name})
                     group_add_list.append(group_name)
             return group_add_list
         except Exception as e:
@@ -122,32 +152,56 @@ class TemplateGroup(ZabbixBase):
                 self._module.exit_json(changed=True)
             self._zapi.templategroup.delete(group_ids)
         except Exception as e:
-            self._module.fail_json(msg="Failed to delete template group(s), Exception: %s" % e)
+            self._module.fail_json(
+                msg="Failed to delete template group(s), Exception: %s" % e
+            )
 
     # get group ids by name
     def get_group_ids(self, template_groups):
         group_ids = []
 
-        group_list = self._zapi.templategroup.get({'output': 'extend', 'filter': {'name': template_groups}})
+        group_list = self._zapi.templategroup.get(
+            {"output": "extend", "filter": {"name": template_groups}}
+        )
         for group in group_list:
-            group_id = group['groupid']
+            group_id = group["groupid"]
             group_ids.append(group_id)
         return group_ids, group_list
+
+    def propagate(self, template_groups, propagate):
+        if LooseVersion(self._zbx_api_version) < LooseVersion("6.2"):
+            return False
+        group_ids, group_list = self.get_group_ids(template_groups)
+        groups = list(map(lambda group_id: {"groupid": group_id}, group_ids))
+        if self._module.check_mode:
+            self._module.exit_json(changed=True)
+        try:
+            self._zapi.templategroup.propagate(
+                {"groups": groups, "permissions": propagate["permissions"]}
+            )
+        except Exception as e:
+            self._module.fail_json(msg="Failed to propagate: %s" % e)
+        return True
 
 
 def main():
     argument_spec = zabbix_utils.zabbix_common_argument_spec()
-    argument_spec.update(dict(
-        template_groups=dict(type='list', required=True, aliases=['template_group'], elements='str'),
-        state=dict(type='str', default="present", choices=['present', 'absent']),
-    ))
-    module = AnsibleModule(
-        argument_spec=argument_spec,
-        supports_check_mode=True
+    argument_spec.update(
+        dict(
+            template_groups=dict(
+                type="list", required=True, aliases=["template_group"], elements="str"
+            ),
+            propagate=dict(
+                type="dict", options=dict(permissions=dict(type="bool", default=False))
+            ),
+            state=dict(type="str", default="present", choices=["present", "absent"]),
+        )
     )
+    module = AnsibleModule(argument_spec=argument_spec, supports_check_mode=True)
 
-    template_groups = module.params['template_groups']
-    state = module.params['state']
+    template_groups = module.params["template_groups"]
+    propagate = module.params["propagate"]
+    state = module.params["state"]
 
     templateGroup = TemplateGroup(module)
 
@@ -162,19 +216,42 @@ def main():
             delete_group_names = []
             templateGroup.delete_template_group(group_ids)
             for group in group_list:
-                delete_group_names.append(group['name'])
-            module.exit_json(changed=True,
-                             result="Successfully deleted template group(s): %s." % ",".join(delete_group_names))
+                delete_group_names.append(group["name"])
+            module.exit_json(
+                changed=True,
+                result="Successfully deleted template group(s): %s."
+                % ",".join(delete_group_names),
+            )
         else:
             module.exit_json(changed=False, result="No template group(s) to delete.")
     else:
         # create template groups
         group_add_list = templateGroup.create_template_group(template_groups)
+        propagated = False
+        if propagate is not None:
+            propagated = templateGroup.propagate(template_groups, propagate)
+
         if len(group_add_list) > 0:
-            module.exit_json(changed=True, result="Successfully created template group(s): %s" % group_add_list)
+            if propagated:
+                module.exit_json(
+                    changed=True,
+                    result="Successfully created template group(s) and propagated config(s) to sub template group(s)",
+                )
+            else:
+                module.exit_json(
+                    changed=True,
+                    result="Successfully created template group(s): %s"
+                    % group_add_list,
+                )
         else:
-            module.exit_json(changed=False)
+            if propagated:
+                module.exit_json(
+                    changed=True,
+                    result="Successfully propagated config(s) to sub template group(s)",
+                )
+            else:
+                module.exit_json(changed=False)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
