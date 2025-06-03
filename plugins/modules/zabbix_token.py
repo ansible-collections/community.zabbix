a696f2a9b16f828337ca3bebae0b017ae150a5aa
Manuel <manuel.pesenti@me.com>
2025-06-03 04:32:14 +0200

    update the logic for update of Zabbix Token (#1543)

--- plugins/modules/zabbix_token.py
+++ plugins/modules/zabbix_token.py
@@ -195,10 +195,10 @@ class Token(ZabbixBase):
             if isinstance(status, bool):
                 if status:
                     if token["status"] != "1":
-                        params["status"] = "1"
+                        params["status"] = "0"
                 else:
                     if token["status"] != "0":
-                        params["status"] = "0"
+                        params["status"] = "1"
 
             if isinstance(expires_at, int) and str(expires_at) != token["expires_at"]:
                 params["expires_at"] = str(expires_at)
