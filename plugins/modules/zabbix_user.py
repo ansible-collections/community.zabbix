bb97d959f953e6089b117f38b1315a5b712b317e
Troy W. <pyrodie18@gmail.com>
2025-06-10 20:09:00 -0400

    fixed user documentation bools (#1549)

--- plugins/modules/zabbix_user.py
+++ plugins/modules/zabbix_user.py
@@ -50,7 +50,7 @@ options:
         description:
             - Override password for the user.
             - Password will not be updated on subsequent runs without setting this value to yes.
-        default: no
+        default: false
         type: bool
     current_passwd:
         description:
@@ -243,7 +243,7 @@ EXAMPLES = r"""
     passwd: password
     lang: en_GB
     theme: blue-theme
-    autologin: no
+    autologin: false
     autologout: "0"
     refresh: "30"
     rows_per_page: "200"
@@ -255,13 +255,13 @@ EXAMPLES = r"""
           - example1@example.com
         period: 1-7,00:00-24:00
         severity:
-          not_classified: no
-          information: yes
-          warning: yes
-          average: yes
-          high: yes
-          disaster: yes
-        active: no
+          not_classified: false
+          information: true
+          warning: true
+          average: true
+          high: true
+          disaster: true
+        active: false
     state: present
 
 - name: delete existing zabbix user.
